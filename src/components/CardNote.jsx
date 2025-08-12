1d81b7e3e762ae61e3c9139c663f5f89bc2e430d
santi carrizo <santiagocarrizo53@gmail.com>
2025-08-11 21:31:06 -0300

    edit listo

--- src/components/CardNote.jsx
+++ src/components/CardNote.jsx
@@ -1,6 +1,8 @@
 import { SquarePen, Trash } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
-const CardNote = ({ title, description, date }) => {
+const CardNote = ({ title, description, date, id }) => {
+  const navigate = useNavigate()
   return (
     <div className="card bg-base-300 w-full">
       <div className="card-body">
@@ -11,7 +13,7 @@ const CardNote = ({ title, description, date }) => {
         <div className="flex justify-between items-center mt-6">
           <time dateTime={date}>{date}</time>
           <div className="flex gap-4">
-            <SquarePen className="text-white cursor-pointer"></SquarePen>
+            <SquarePen className="text-white cursor-pointer" onClick={() => navigate(`editNote/${id}`)}></SquarePen>
             <Trash className="text-red-400 cursor-pointer"></Trash>
           </div>
         </div>
--- src/pages/EditNotePage.jsx
+++ src/pages/EditNotePage.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { useNavigate, useParams } from "react-router-dom";
+import { useState } from "react";
+import { useEffect } from "react";
+import NoteForm from "../components/NoteForm";
+
+// /editNote/:id
 
 const EditNotePage = () => {
-  return (
-    <div><h1>Edit Note Page</h1></div>
-  )
-}
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const [noteData, setNoteData] = useState({ title: "", content: "" });
 
-export default EditNotePage
+  useEffect(() => {
+    const fetchNote = async () => {
+      try {
+        const res = await axios.get(
+          `${import.meta.env.VITE_API_URL}/api/notes/${id}`
+        );
+        setNoteData(res.data);
+      } catch (error) {
+        console.error(error);
+        toast.error("Error al cargar la nota");
+      }
+    };
+    fetchNote();
+  }, [id]);
+
+   const handleUpdate = async (updatedNote) => {
+    try {
+      const res = await axios.put(`${import.meta.env.VITE_API_URL}/api/notes/${id}`, updatedNote);
+      if (res.status !== 200) {
+        throw new Error("Error al actualizar la nota");
+      }
+      toast.success("¡Nota actualizada con éxito!", {
+        position: "bottom-center",
+        autoClose: 3000,
+        theme: "colored",
+      });
+      navigate("/");
+    } catch (error) {
+      console.error(error);
+      toast.error("No se pudo actualizar la nota");
+    }
+  };
+
+  return (
+    <div>
+      <NoteForm 
+        onSubmit={handleUpdate}
+        initialDate={noteData}
+      />
+    </div>
+  );
+};
+
+export default EditNotePage;
